c63403ac649742e312d21e1c8699eec78b0b429a
Sofia Martinez <sofiacaro1284@gmail.com>
2025-01-31 09:04:19 -0500

    added useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
 const clientID = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchInput, setSearchInput] = useState(""); //Use for API request
+  const [accessToken, setAccessToken] = useState("");
+
+  //Hook
+  useEffect( () => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientID +
+        "&client_secret=" +
+        clientSecret,
+      };
+      fetch("https://accounts.spotify.com/api/token", authParams)
+        .then((result) => result.json())
+        .then((data) => {
+          setAccessToken(data.access_token);
+        });
+    },
+  []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <InputGroup>
+        <FormControl
+          placeholder="artist"
+          type="input"
+          aria-label="artist"
+          onKeyDown={} // search function
+          onChange={} // setSearch
+          style={{
+            width: "300px",
+            height: "35px",
+            borderWidth: "0px",
+            borderStyle: "solid",
+            borderRadius: "5px",
+            marginRight: "10px",
+            paddingLeft: "10px",
+          }}
+        />
+
+        <Button onClick={{}}>Search</Button>
+      </InputGroup>
+    </Container>
+  );
 }
 
 export default App
