5aaf9fad0023822537687e6ea675b7614d7122b1
Sofia Martinez <sofiacaro1284@gmail.com>
2025-01-30 23:47:05 -0500

    declared keys

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const clientID = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
   const [count, setCount] = useState(0)
