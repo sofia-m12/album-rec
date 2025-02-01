2fbba5915436a5c4ce6070b712f0fd3bca4cfe33
Sofia Martinez <sofiacaro1284@gmail.com>
2025-02-01 17:12:55 -0500

    Added display cards for the artists albums

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,10 @@ function App() {
 
   return (
     <Container>
+      <Container style={{fontFamily: "cursive",
+        fontSize: "40px",
+        }}>Enrich Your Music Taste!
+      </Container>
       <InputGroup>
         <FormControl
           placeholder="artist"
@@ -81,8 +85,8 @@ function App() {
           style={{
             width: "300px",
             height: "35px",
-            borderWidth: "0px",
-            borderStyle: "solid",
+            borderWidth: "1px",
+            borderStyle: "dotted",
             borderRadius: "5px",
             marginRight: "10px",
             paddingLeft: "10px",
@@ -91,6 +95,74 @@ function App() {
 
         <Button onClick={search}>Search</Button>
       </InputGroup>
+      <Container>
+      <Row
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          alignContent: "center",
+        }}>
+        {
+          //Displaying each album as a Card
+          albums.map((album) => {
+            return (
+              <Card
+                key={album.id}
+                style={{
+                  backgroundColor: "black",
+                  margin: "10px",
+                  borderRadius: "1%",
+                  marginBottom: "30px",
+                }}
+              >
+                <Card.Img
+                  width={180}
+                  src={album.images[0].url}
+                  style={{
+                    borderRadius: "60%",
+                  }}
+                />
+                <Card.Body>
+                  <Card.Title
+                    style={{
+                      whiteSpace: "wrap",
+                      fontWeight: "normal",
+                      maxWidth: "fit-content",
+                      fontSize: "20px",
+                      marginTop: "10px",
+                      color: "white",
+                      fontFamily: "cursive",
+                    }}>
+                    {album.name}
+                  </Card.Title>
+                  <Card.Text
+                    style={{color: "white", fontWeight: "lighter", fontFamily: "cursive",}}>
+                    Released: {album.release_date}
+                  </Card.Text>
+                  <Button
+                    href={album.external_urls.spotify}
+                    style={{
+                      backgroundColor: "black",
+                      color: "blueviolet",
+                      fontWeight: "normal",
+                      fontSize: "15px",
+                      borderRadius: "5px",
+                      borderWidth: "5px",
+                      borderColor: "white",
+                      padding: "10px",
+                      fontFamily: "cursive",
+                    }}>
+                    LINK!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })
+        }
+      </Row>
+      </Container>
     </Container>
   );
 }
