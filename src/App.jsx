430af166467c83f044f7f83802089599140f99b3
Sofia Martinez <sofiacaro1284@gmail.com>
2025-02-05 19:18:05 -0500

    updated the expanded albums logic & the album card UI

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         "Content-Type": "application/json",
         Authorization: "Bearer " + accessToken,
       },
-    }
+    };
     
     //GET artist
     const artistID = await fetch(
@@ -60,63 +60,59 @@ function App() {
       .then((data) => {
         setAlbums(data.items);  
       });
+  }
 
-      //GET album features
-      async function getAlbumTracks(albumID) {
-        try{
-          let trackParams = {
+  //GET album features
+  async function getAlbumTracks(albumID) {
+    try{
+      let trackParams = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + accessToken,
+        },
+      };
+
+      //Getting the tracks on the specified album
+      const tracks = await fetch(
+        "https://api.spotify.com/v1/albums/" + albumID + "/tracks",
+        trackParams
+      )
+      .then((result) => result.json())
+      .then((data) => data.items);
+
+      //Get all the artists on the album
+      const artistData = new Map();
+      tracks.items.forEach((track) => {
+        track.artists.forEach((artist) => {
+          artistData.set(artist.id, artist.name);
+        });
+      });
+
+      //get theses artists albums
+      const featuredArtistsAlbums = [];
+      for(const [fartistID, fartistName] of artistData.entries()) {
+        const falbumsResponse = await fetch(
+          "https://api.spotify.com/v1/artists/" + fartistID + "/albums?include_groups=album&market=US&limit=5",
+          {
             method: "GET",
             headers: {
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + accessToken,
+              Authorization: `Bearer ${accessToken}`,
             },
-          };
-
-          //Getting the tracks on the specified album
-          const tracks = await fetch(
-            "https://api.spotify.com/v1/albums/" + albumID + "/tracks",
-            trackParams
-          )
-          .then((result) => result.json)
-          .then((data) => data.items);
-
-          //Get all the artists on the album
-          const artistData = new Map();
-          tracks.items.forEach((track) => {
-            track.artists.forEach((artist) => {
-              artistData.set(artist.id, artist.name);
-            });
-          });
-
-          //get theses artists albums
-          const featuredArtistsAlbums = [];
-          for(const [fartistID, fartistName] of artistData.entries()) {
-            const falbumsResponse = await fetch(
-              "https://api.spotify.com/v1/artists/" + fartistID + "/albums?include_groups=album&market=US&limit=5",
-              {
-                method: "GET",
-                headers: {
-                  Authorization: `Bearer ${accessToken}`,
-                },
-              }
-            );
-
-            const albumData = await falbumsResponse.json();
-            featuredArtistsAlbums.push({
-              fartistName,
-              albums: albumData.items,
-            });
           }
-          //Convert the map entries to array for rendering
-          setExpandedAlbums(featuredArtistsAlbums);
-        } catch(error) {
-        console.error("Error fetching data: ", error);
-        }
-      }
+        );
 
-      console.log("search input: " + searchInput);
-      console.log("artist id: " + artistID);
-      console.log("albums: " + setAlbums);
+        const albumData = await falbumsResponse.json();
+        featuredArtistsAlbums.push({
+          fartistName,
+          albums: albumData.items,
+        });
+      }
+      //Convert the map entries to array for rendering
+      setExpandedAlbums(featuredArtistsAlbums);
+    } catch(error) {
+    console.error("Error fetching data: ", error);
+    }
   }
 
   return (
@@ -150,44 +146,127 @@ function App() {
         <Button onClick={search}>Search</Button>
       </InputGroup>
       <Container>
-      <Row
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "space-around",
-          alignContent: "center",
-        }}>
-        {
-          //Displaying each album as a Card
-          albums.map((album) => {
-            return (
+        <Row
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            alignContent: "center",
+          }}>
+          {
+            //Displaying each album as a Card
+            albums.map((album) => {
+              return (
+                <Card
+                  key={album.id}
+                  style={{
+                    backgroundColor: "#D3F1DF",
+                    margin: "10px",
+                    borderRadius: "3%",
+                    marginBottom: "30px",
+                    width: "200px",
+                    height: "375px",
+                  }}
+                >
+                  <Card.Title
+                      style={{
+                        whiteSpace: "wrap",
+                        fontWeight: "normal",
+                        maxWidth: "100%",
+                        fontSize: "20px",
+                        marginTop: "5px",
+                        color: "black",
+                        fontFamily: "sans-serif",
+                      }}>
+                      {album.name}
+                  </Card.Title>
+                  <Card.Img 
+                    width={180}
+                    src={album.images[0].url}
+                    style={{
+                      borderRadius: "3%",
+                      marginTop: "10px",
+                      marginBottom: "15px",
+                    }}
+                  />
+                  <Card.Body>
+                      <Button
+                        style={{
+                          maxWidth: "fit-content",
+                          marginLeft: "5px",
+                          borderRadius: "70%",
+                          backgroundColor: "#85A98F",
+                        }}
+                        onClick={getAlbumTracks(album.id)}>Expand
+                      </Button>
+
+                    <Card.Text
+                      style={{color: "black", fontWeight: "lighter", fontFamily: "sans-serif",}}>
+                      Released: {album.release_date}
+                    </Card.Text>
+                    <Button
+                      href={album.external_urls.spotify}
+                      style={{
+                        backgroundColor: "#181C14",
+                        color: "#1ED760",
+                        fontWeight: "normal",
+                        fontSize: "10px",
+                        borderRadius: "5px",
+                        borderWidth: "5px",
+                        borderColor: "white",
+                        padding: "10px",
+                        fontFamily: "sans-serif",
+                      }}>
+                      SPOTIFY
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })
+          }
+        </Row>
+      </Container>
+
+      {expandedAlbums && expandedAlbums.length > 0 && (
+        <Container>
+          <h1 style={{fontFamily: "sans-serif", marginTop: "15px",}}>
+          Featured Artists Albums:
+          </h1>
+         <Row 
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            alignContent: "center",
+          }}>
+          {expandedAlbums.map((eArtist) => (
+            eArtist.albums.map((eAlbum) => (
               <Card
-                key={album.id}
+                key={eAlbum.id}
                 style={{
-                  backgroundColor: 'purple',
+                  backgroundColor: "#D3F1DF",
                   margin: "10px",
                   borderRadius: "3%",
                   marginBottom: "30px",
                   width: "200px",
                   height: "375px",
-                }}
-              >
+                }}>
                 <Card.Title
-                    style={{
-                      whiteSpace: "wrap",
-                      fontWeight: "normal",
-                      maxWidth: "100%",
-                      fontSize: "20px",
-                      marginTop: "5px",
-                      color: "white",
-                      fontFamily: "sans-serif",
-                    }}>
-                    {album.name}
-                </Card.Title>
-                <Card.Img 
-                  width={180}
-                  src={album.images[0].url}
+                      style={{
+                        whiteSpace: "wrap",
+                        fontWeight: "normal",
+                        maxWidth: "100%",
+                        fontSize: "20px",
+                        marginTop: "5px",
+                        color: "black",
+                        fontFamily: "sans-serif",
+                      }}>
+                      {eAlbum.name}
+                  </Card.Title>
+                <Card.Img
+                  src={eAlbum.images[0].url}
                   style={{
                     borderRadius: "3%",
                     marginTop: "10px",
@@ -195,41 +274,44 @@ function App() {
                   }}
                 />
                 <Card.Body>
-                    <Button
-                      style={{
-                        maxWidth: "fit-content",
-                        marginLeft: "5px",
-                        borderRadius: "70%",
-                      }}
-                      onClick={getAlbumTracks(album.id)}>Expand
-                    </Button>
+                <Button
+                        style={{
+                          maxWidth: "fit-content",
+                          marginLeft: "5px",
+                          borderRadius: "70%",
+                          backgroundColor: "#85A98F",
+                        }}
+                        onClick={getAlbumTracks(eAlbum.id)}>Expand
+                      </Button>
 
-                  <Card.Text
-                    style={{color: "white", fontWeight: "lighter", fontFamily: "sans-serif",}}>
-                    Released: {album.release_date}
-                  </Card.Text>
-                  <Button
-                    href={album.external_urls.spotify}
-                    style={{
-                      backgroundColor: "black",
-                      color: "#1ED760",
-                      fontWeight: "normal",
-                      fontSize: "10px",
-                      borderRadius: "5px",
-                      borderWidth: "5px",
-                      borderColor: "white",
-                      padding: "10px",
-                      fontFamily: "sans-serif",
-                    }}>
-                    SPOTIFY
-                  </Button>
+                    <Card.Text
+                      style={{color: "black", fontWeight: "lighter", fontFamily: "sans-serif",}}>
+                      Released: {eAlbum.release_date}
+                    </Card.Text>
+                    <Button
+                      href={eAlbum.external_urls.spotify}
+                      style={{
+                        backgroundColor: "#181C14",
+                        color: "#1ED760",
+                        fontWeight: "normal",
+                        fontSize: "10px",
+                        borderRadius: "5px",
+                        borderWidth: "5px",
+                        borderColor: "white",
+                        padding: "10px",
+                        fontFamily: "sans-serif",
+                      }}>
+                      SPOTIFY
+                    </Button>
                 </Card.Body>
               </Card>
-            );
-          })
-        }
-      </Row>
-      </Container>
+            ))
+          ))
+          }
+         </Row>
+       </Container>
+       )
+      }
     </Container>
   );
 }
